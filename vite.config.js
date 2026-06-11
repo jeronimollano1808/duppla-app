fca0d707fb5351dba1b3071efce263cd842f89d7
Jeronimo Arroyave <jarroyave@MacBook-Air-de-Jeronimo.local>
2026-06-10 21:38:51 -0500

    Fix Safari iOS 26

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy({
-      targets: ['ios >= 13', 'safari >= 13'],
-      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
-    })
-  ],
+  plugins: [react()],
   build: {
-    target: 'es2015'
+    target: ['es2020', 'safari14'],
+    sourcemap: false
   }
 })
