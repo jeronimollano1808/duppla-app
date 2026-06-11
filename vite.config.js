d9a256a911a654899b5768c42fda70cdcc9fc364
Jeronimo Arroyave <jarroyave@MacBook-Air-de-Jeronimo.local>
2026-06-10 21:46:11 -0500

    Split chunks iOS fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: ['es2020', 'safari14'],
-    sourcemap: false
+    target: ['es2019', 'safari13'],
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react': ['react', 'react-dom'],
+          'vendor-firebase': ['firebase/app', 'firebase/firestore', 'firebase/auth'],
+          'vendor-charts': ['recharts']
+        }
+      }
+    }
   }
 })
